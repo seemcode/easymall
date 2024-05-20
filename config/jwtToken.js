081f9a629182535caea1b873cffa60dae401d95c
waseemcode <directwaseem@gmail.com>
2024-05-20 13:26:12 +0500

    checking

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -4,4 +4,8 @@ const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
+const generateToken1 = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
 module.exports = { generateToken };
